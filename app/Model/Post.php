872b0fbc7c0685ea1ced5ae4a95237b2a58b906a
Daniel Brito <danielb1989@gmail.com>
2020-02-05 16:16:15 -0300

    Configuração inicial, criando blog conforme manual encontrado na versão 2x

---
+++ app/Model/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+    class Post extends AppModel {
+
+        public $name = 'Post';
+        
+        public $validate = array(
+            'title' => array(
+                'rule' => 'notBlank'
+            ),
+            'body' => array(
+                'rule' => 'notBlank'
+            )
+        );
+        
+        public function isOwnedBy($post, $user) {
+            return $this->field('id', array('id' => $post, 'user_id' => $user)) === $post;
+        }
+        
+    }
